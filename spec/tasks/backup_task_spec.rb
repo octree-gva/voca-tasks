2e934fe5e2da94884f3c26466fae82d7e2a2672c
Hadrien Froger <e3k8y9i0k3s8o9x4@octreea17.slack.com>
2022-03-17 04:01:30 +0000

    chore: refactor files and splits class.

--- spec/tasks/backup_task_spec.rb
+++ spec/tasks/backup_task_spec.rb
@@ -1,45 +1,33 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "decidim/vocacity_gem_tasks"
 
-describe "VocacityGemTasks" do
+describe "VocacityGemTasks::AppBackup" do
+  context "#run!" do
+    let(:dir_klass) {class_double("Dir").as_stubbed_const()}
 
-  context "when initialize AppBackup" do 
-    let!(:logger) { instance_double("Logger") }
-    let(:app_backup) { Decidim::VocacityGemTasks::AppBackup }
-    let!(:backup_runner) { Decidim::VocacityGemTasks::AppBackup.new }
-    let(:check_pgpass) { backup_runner.send(:check_pgpass) }
-    let(:file_pgpass) { File.exists?("/root/.pgpass") }
-    let!(:dir) { Dir.mktmpdir }
-    let(:dump_file) { backup_runner.send(:dump_database, dir) }
-    let(:compressed_file) { backup_runner.send(:compress_uploads, dir) }
-    let(:file_metadata) { backup_runner.send(:generate_metadatas, dir) }
-
-    before do
-      expect(logger).to receive(:info).with(anything)
+    it "creates a temp directory" do
+      expect(dir_klass).to receive(:mktmpdir).with(any_args)
+      VocacityGemTasks::AppBackup.new.run!
     end
 
-    it "should inicialize AppBackup" do
-      logger.info("mock")
-      expect(app_backup).to receive(:new).and_return(nil)
-      app_backup.new
-      expect(check_pgpass).to be nil
-      expect(file_pgpass).to be true
+    it "dump_database,compress_uploads and generate_metadatas with the fresh dir" do
+      class DirStub
+        def self.mktmpdir(&block)
+          block.call("my-dirname")
+        end
+      end
+      runner = VocacityGemTasks::AppBackup.new
+      expect(runner).to receive(:dump_database).with("my-dirname")
+      expect(runner).to receive(:compress_uploads).with("my-dirname")
+      expect(runner).to receive(:generate_metadatas).with("my-dirname")
+      
+      stub_const("Dir", DirStub)
+      allow(runner).to receive(:dump_database).and_return(nil)
+      allow(runner).to receive(:compress_uploads).and_return(nil)
+      allow(runner).to receive(:generate_metadatas).and_return(nil)
+      allow(runner).to receive(:with_backup_dir).and_return(nil)
+      runner.run!
     end
-  
-    it "should call run!" do
-      logger.info("mock")
-      expect(backup_runner).to receive(:run!)
-      backup_runner.run!
-    end
-
-    it "should execute private methods" do
-      logger.info("mock")
-      expect(dump_file).to match(/\.dump/)
-      expect(compressed_file).to match(/\.tar\.gz/)
-      expect(file_metadata).to match(/metadatas\.json/)
-    end
-
   end
 end
