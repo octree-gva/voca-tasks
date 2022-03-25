647408948db6e99769057a1ea447fbbe6135b407
Renato da Silva de Deus <rsdeus@gmail.com>
2022-03-25 16:06:49 -0300

    test(AppSendToS3): create tests

--- lib/decidim/vocacity_gem_tasks/backup/app_send_to_s3.rb
+++ lib/decidim/vocacity_gem_tasks/backup/app_send_to_s3.rb
@@ -5,8 +5,24 @@ module Decidim
         logger.info "⚙️ starts send to S3"
       end
 
-      def check_backup_file
+      def get_backup_file
       end
+
+      def get_vocacity_bucket
+      end
+
+      def create_vocacity_client_dir
+      end
+
+      def send_backup_file
+      end
+
+      def get_backup_files(client_name)
+      end
+      
+      def remove_backup_file(client_name, backup_file_name)
+      end
+      
     end
   end
 end
